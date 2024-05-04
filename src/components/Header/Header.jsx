55a1703bcf8245859e463b621f63818adb4308f8
wsfer <93054415+wsfer@users.noreply.github.com>
2024-05-03 23:57:44 -0300

    feat: create Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,39 @@
+import { Link, NavLink } from 'react-router-dom';
+import Container from '../Container/Container';
 import styles from './Header.module.scss';
 
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header className={styles.header}>
+      <Container>
+        <div className={styles.content}>
+          <Link className={styles.logo} to="/">
+            <span className={styles.orange}>Fashion</span>
+            <span className={styles.white}>Store</span>
+          </Link>
+          <nav>
+            <ul className={styles.list}>
+              <li>
+                <NavLink className={styles.link} to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className={styles.link} to="/products">
+                  Products
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className={styles.link} to="/cart">
+                  Cart
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </Container>
+    </header>
+  );
 }
 
 export default Header;
