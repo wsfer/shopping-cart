c4abae49b1f3f1505b0115438612612f7b72bfc4
wsfer <93054415+wsfer@users.noreply.github.com>
2024-05-04 23:55:51 -0300

    feat: change font size variables to be more responsive

--- src/pages/Home/Categories.jsx
+++ src/pages/Home/Categories.jsx
@@ -6,37 +6,35 @@ function Categories() {
   return (
     <section className={styles.categories}>
       <Container>
-        <div className={styles.content}>
-          <h2 className={styles.title}>Categories</h2>
-          <nav>
-            <ul className={styles.list}>
-              <li className={styles.listItem}>
-                <Link
-                  className={`${styles.link} ${styles.linkOne}`}
-                  to="/products/women-clothes"
-                >
-                  Women's clothes
-                </Link>
-              </li>
-              <li className={styles.listItem}>
-                <Link
-                  className={`${styles.link} ${styles.linkTwo}`}
-                  to="/products/men-clothes"
-                >
-                  Men's clothes
-                </Link>
-              </li>
-              <li className={styles.listItem}>
-                <Link
-                  className={`${styles.link} ${styles.linkThree}`}
-                  to="/products/jewelry"
-                >
-                  Jewelry
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
+        <h2 className={styles.title}>Categories</h2>
+        <nav>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>
+              <Link
+                className={`${styles.link} ${styles.linkOne}`}
+                to="/products/women-clothes"
+              >
+                Women's clothes
+              </Link>
+            </li>
+            <li className={styles.listItem}>
+              <Link
+                className={`${styles.link} ${styles.linkTwo}`}
+                to="/products/men-clothes"
+              >
+                Men's clothes
+              </Link>
+            </li>
+            <li className={styles.listItem}>
+              <Link
+                className={`${styles.link} ${styles.linkThree}`}
+                to="/products/jewelry"
+              >
+                Jewelry
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </Container>
     </section>
   );
