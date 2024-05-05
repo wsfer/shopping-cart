79026616674956de9902151d16586837eb54c02f
wsfer <93054415+wsfer@users.noreply.github.com>
2024-05-04 23:40:45 -0300

    feat: create Home page component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,15 @@
-import styles from './Home.module.scss';
+import Hero from './Hero';
+import About from './About';
+import Categories from './Categories';
 
 function Home() {
-  return <main>Home</main>;
+  return (
+    <>
+      <Hero />
+      <About />
+      <Categories />
+    </>
+  );
 }
 
 export default Home;
