e93ca038f430d88f3dddb28a6e6afc075c948c8b
wsfer <93054415+wsfer@users.noreply.github.com>
2024-05-28 16:15:07 -0300

    test: add mor tests for Sidebar component

--- src/components/Sidebar/Sidebar.test.jsx
+++ src/components/Sidebar/Sidebar.test.jsx
@@ -12,6 +12,7 @@ test('Should have links within a navigation to products categories', async () =>
 
   render(<Sidebar />, { wrapper: routerWrapper });
   const nav = screen.getByRole('navigation', { name: /categories/i });
+  const general = within(nav).getByRole('link', { name: 'General' });
   const menClothes = within(nav).getByRole('link', {
     name: "Men's clothing",
   });
@@ -19,6 +20,9 @@ test('Should have links within a navigation to products categories', async () =>
   const womenClothes = within(nav).getByRole('link', {
     name: "Women's clothing",
   });
+
+  await user.click(general);
+  expect(window.location.pathname).toBe('/products');
 
   await user.click(menClothes);
   expect(window.location.pathname).toBe('/products/men-clothing');
@@ -28,4 +32,53 @@ test('Should have links within a navigation to products categories', async () =>
 
   await user.click(womenClothes);
   expect(window.location.pathname).toBe('/products/women-clothing');
+});
+
+/**
+ * It's not possible to test CSS properties from CSS files, so the number of classes is tested here
+ *    hidden = 2 CSS Classes (default class + hidden class)
+ *    visible = 1 CSS Class (default class only)
+ *
+ * How it's hidden with CSS is irrelevant here
+ */
+describe('Responsivity functionality', () => {
+  describe('On render', () => {
+    test('Should render nav toggler button', () => {
+      render(<Sidebar />, { wrapper: routerWrapper });
+      const toggler = screen.getByRole('button', { name: 'Sidebar' });
+      expect(toggler).toBeInTheDocument();
+    });
+
+    test('Navigation should be hidden by default with CSS', () => {
+      render(<Sidebar />, { wrapper: routerWrapper });
+      const nav = screen.getByRole('navigation', { name: /categories/i });
+      expect(nav.classList.length).toBe(2); // default class + hidden class
+    });
+  });
+
+  describe('On interaction', () => {
+    test('Nav toggler should show navigation', async () => {
+      const user = userEvent.setup();
+
+      render(<Sidebar />, { wrapper: routerWrapper });
+      const toggler = screen.getByRole('button', { name: 'Sidebar' });
+      const nav = screen.getByRole('navigation', { name: /categories/i });
+
+      await user.click(toggler);
+      expect(nav.classList.length).toBe(1); // default class only
+    });
+
+    test('Nav toggler should hide navigation again', async () => {
+      const user = userEvent.setup();
+
+      render(<Sidebar />, { wrapper: routerWrapper });
+      const toggler = screen.getByRole('button', { name: 'Sidebar' });
+      const nav = screen.getByRole('navigation', { name: /categories/i });
+
+      await user.click(toggler);
+      await user.click(toggler);
+
+      expect(nav.classList.length).toBe(2); // default class + hidden class
+    });
+  });
 });
