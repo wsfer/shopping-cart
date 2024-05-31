2a8ff3b9e5d40194b9df52bb700a63d40c0eb263
wsfer <93054415+wsfer@users.noreply.github.com>
2024-05-31 16:19:21 -0300

    test: add tests for event functions on Popup component

--- src/components/Popup/Popup.test.jsx
+++ src/components/Popup/Popup.test.jsx
@@ -1,5 +1,6 @@
 import { createRef } from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Popup from './Popup';
 
 /**
@@ -14,16 +15,80 @@ beforeEach(() => {
   });
 });
 
-test('Should render children components', () => {
-  const ref = createRef();
+// Render popup opened
+const setup = (onAccept, onRefuse, onClose) => {
+  const popup = createRef();
 
   render(
-    <Popup ref={ref}>
+    <Popup
+      ref={popup}
+      onAccept={onAccept}
+      onRefuse={onRefuse}
+      onClose={onClose}
+    >
       <div data-testid="children">Hello</div>
     </Popup>
   );
 
-  ref.current.showModal();
+  popup.current.showModal();
+
+  return popup;
+};
+
+test('Should render children components', () => {
+  setup();
   const children = screen.getByTestId('children');
   expect(children).toBeVisible();
+});
+
+describe('When onAccept function is defined', () => {
+  test('should render accept button to call that function', async () => {
+    const onAccept = vi.fn();
+    const user = userEvent.setup();
+
+    setup(onAccept);
+
+    const acceptBtn = screen.getByRole('button', { name: /confirm/i });
+    await user.click(acceptBtn);
+    expect(onAccept).toHaveBeenCalled();
+  });
+});
+
+describe('When onRefuse is defined', () => {
+  test('should render refuse button to call that function', async () => {
+    const onRefuse = vi.fn();
+    const user = userEvent.setup();
+
+    setup(undefined, onRefuse);
+
+    const refuseBtn = screen.getByRole('button', { name: /cancel/i });
+    await user.click(refuseBtn);
+    expect(onRefuse).toHaveBeenCalled();
+  });
+});
+
+describe('When onClose is defined', () => {
+  test('cancel button should call that function', async () => {
+    const onClose = vi.fn();
+    const user = userEvent.setup();
+
+    setup(undefined, undefined, onClose);
+
+    const closeBtn = screen.getByRole('button', { name: /close/i });
+    await user.click(closeBtn);
+    expect(onClose).toHaveBeenCalled();
+  });
+});
+
+describe('When event functions are undefined', () => {
+  test('should render only close button', () => {
+    setup();
+    const closeBtn = screen.queryByRole('button', { name: /close/i });
+    const acceptBtn = screen.queryByRole('button', { name: /confirm/i });
+    const refuseBtn = screen.queryByRole('button', { name: /cancel/i });
+
+    expect(closeBtn).toBeInTheDocument();
+    expect(acceptBtn).toBeNull();
+    expect(refuseBtn).toBeNull();
+  });
 });
