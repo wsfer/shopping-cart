0dd707d21e998db02e25d5bb19a1f9eb4ebc5c0b
wsfer <93054415+wsfer@users.noreply.github.com>
2024-06-03 17:16:43 -0300

    feat: create error page for not found routes

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom';
 import styles from './ErrorPage.module.scss';
 
 function ErrorPage() {
-  return <main>404</main>;
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>404</h1>
+      <p className={styles.paragraph}>Are you lost?</p>
+      <Link to="/" className={styles.button}>
+        Start again
+      </Link>
+    </main>
+  );
 }
 
 export default ErrorPage;
