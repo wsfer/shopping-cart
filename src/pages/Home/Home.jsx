6155e3b6aff664ad3fefa2f3de76a6eeb2635e17
wsfer <93054415+wsfer@users.noreply.github.com>
2024-06-04 11:57:02 -0300

    fix: homepage responsivity issues on lower screens

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,15 @@
 import Hero from './Hero';
 import About from './About';
 import Categories from './Categories';
+import styles from './Home.module.scss';
 
 function Home() {
   return (
-    <>
+    <div className={styles.home}>
       <Hero />
       <About />
       <Categories />
-    </>
+    </div>
   );
 }
 
